7ead6a7adc42cd53a3a6d5e7ac7056a6c6c3822f
DeniOS777 <Denios777@gmail.com>
2022-11-14 14:54:33 +0200

    changed styles

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getChat, sendMessage } from 'redux/chatOperations';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export const ChatPage = () => {
   const dispatch = useDispatch();
@@ -16,17 +18,35 @@ export const ChatPage = () => {
   }, []);
 
   return (
-    <div>
-      <p>Hello</p>
-      <form autoComplete="off" onSubmit={handleSubmit}>
-        <input
-          id="message"
-          type="text"
-          name="message"
-          placeholder="Enter message..."
-        />
-        <button type="submit">Send Message</button>
-      </form>
+    <div
+      style={{
+        margin: '0 auto',
+        paddingLeft: '15px',
+        paddingRight: '15px',
+        maxWidth: '550px',
+      }}
+    >
+      <h1 style={{ textAlign: 'center', fontSize: '26px' }}>
+        You are welcom to the Google chat👋
+      </h1>
+      <Form
+        className="d-flex gap-md-2"
+        onSubmit={handleSubmit}
+        autoComplete="off"
+      >
+        <Form.Group className="mb-3 d-flex flex-grow-1">
+          <Form.Control
+            id="message"
+            type="text"
+            name="message"
+            placeholder="Enter your message..."
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Send
+        </Button>
+      </Form>
     </div>
   );
 };
